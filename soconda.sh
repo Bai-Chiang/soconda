d7af072a535800469d3a5c297620ba3f2cb29a54
Bai-Chiang <contact@bai-chiang.com>
2024-10-13 13:16:17 -0700

    Change default env name

--- soconda.sh
+++ soconda.sh
@@ -66,8 +66,8 @@ if [ -z "${version}" ]; then
 fi
 
 if [ -z "${envname}" ]; then
-    echo "Environment root name not specified, using \"soconda\""
-    envname="soconda"
+    echo "Environment root name not specified, using \"SOOPERCOOL\""
+    envname="SOOPERCOOL"
 fi
 # The full environment name, including the root and version.
 if [ "${envname}" = "base" ]; then
